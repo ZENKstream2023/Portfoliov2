9d658bc100a674859c8161af917f4553c9cc16c6
ZENKstream2023 <zenkstreamhelp@gmail.com>
2024-04-16 12:31:19 +0200

    modified:   RRHH_backend/.env 	modified:   RRHH_backend/index.js 	deleted:    RRHH_backend/index_AVANCED.js 	modified:   Vue_FrontendV2/package-lock.json 	modified:   Vue_FrontendV2/package.json 	modified:   Vue_FrontendV2/public/index.html

--- RRHH_backend/index.js
+++ RRHH_backend/index.js
@@ -32,14 +32,26 @@ app.use(verifyToken);
 app.use(cors(corsOptions));
 */
 app.use(bodyParser.json());
+// Content Security Policy
+app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', "script-src 'self' https://cdn.jsdelivr.net/; img-src 'self' http://localhost:3200;");
+    next();
+});
 
 // Sirve el frontend estático desde /var/www/frontend/dist
 app.use(express.static(path.join(__dirname, "../Vue_FrontendV2/dist")));
-// Definir la política CSP
-app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "script-src 'self' https://cdn.jsdelivr.net;");
+// Middleware para servir archivos CSS con el tipo MIME correcto
+app.use("/assets/css", (req, res, next) => {
+    res.setHeader("Content-Type", "text/css");
     next();
-});
+}, express.static(path.join(__dirname, "assets/css")));
+
+// Middleware para servir archivos JS con el tipo MIME correcto
+app.use("/assets/js", (req, res, next) => {
+    res.setHeader("Content-Type", "text/javascript");
+    next();
+}, express.static(path.join(__dirname, "assets/js")));
+
   // Manejar todas las demás rutas y redirigirlas al archivo de entrada del frontend
 // Middleware para excluir rutas que comiencen por "/api"
 app.use((req, res, next) => {
